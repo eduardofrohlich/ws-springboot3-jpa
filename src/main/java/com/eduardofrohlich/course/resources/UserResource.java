2f89d011b0679a6c646dbd44dee985d5796bce5e
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-08-10 22:30:13 -0300

    User entity and resource

---
+++ src/main/java/com/eduardofrohlich/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.eduardofrohlich.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardofrohlich.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Edu", "edu@gmail.com", "9999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}

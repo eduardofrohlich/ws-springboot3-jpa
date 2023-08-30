ff20724c79bb9a0491afb2a3675620867bb3bae4
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-08-30 15:02:28 -0300

    JPA repository, depency injection, database seeding

---
+++ src/main/java/com/eduardofrohlich/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eduardofrohlich.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduardofrohlich.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}

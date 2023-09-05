5ca85e62519192c5cdbe6e6689157345502a3e1c
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-09-05 15:19:17 -0300

    Order, Instant, ISO8601, order seed

---
+++ src/main/java/com/eduardofrohlich/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.eduardofrohlich.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduardofrohlich.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
+	
